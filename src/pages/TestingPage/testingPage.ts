fb76127e64449d79911bf7e7fd36a6811a8f8091
oOBlinkOo <rivensona@gmail.com>
2017-02-13 11:26:30 -0600

    yea

--- src/pages/TestingPage/testingPage.ts
+++ src/pages/TestingPage/testingPage.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HomePage} from '../home/home';
 
 import { NavController } from 'ionic-angular';
 
@@ -18,6 +19,11 @@ showmessageWrongPassword :boolean = false;
 validateLogin(){
   if (this.username=='123' && this.password=='123' ){
     console.log('got it');
+     this.navCtrl.push(HomePage, {
+      username:this.username,
+      password:this.password
+    
+    });
 }else {
     console.log('password incorrecto');
     this.showmessageWrongPassword = true;
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController ,NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+      username: string;
+      password:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController ,public params:NavParams) {
+    this.username = params.get('username');
+    this.password = params.get('password');
+  }
 
+  ngOnInit(){
+    console.log(this.username,this.password);
   }
 
 }
